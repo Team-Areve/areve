1fcffaa9022f4d4bb82c63a342acac12ca8e3905
K <dkh1001@outlook.kr>
2022-01-21 00:24:59 +0900

    Arrange Category Icons

--- src/components/main/Category.js
+++ src/components/main/Category.js
@@ -1,135 +1,74 @@
 import React from "react";
 import styled from "styled-components";
 import { palette } from "../../lib/styles/palette.js";
+import CategoryIcon from "./CategoryIcon.js";
 
 const Category = () => {
-
   return (
     <CategoryContainer>
-      <CategoryItem>
-        <button>
-          <i></i>
-          <div>주거용</div>
-        </button>
-      </CategoryItem>
-      
-      <CategoryItem>
-        <button>
-          <i></i>
-          <div>사무실</div>
-        </button>
-      </CategoryItem>
-      
-      <CategoryItem>
-        <button>
-          <i></i>
-          <div>회의실</div>
-        </button>
-      </CategoryItem>
-      
-      <CategoryItem>
-        <button>
-          <i></i>
-          <div>연습실</div>
-        </button>
-      </CategoryItem>
-      
-      <CategoryItem>
-        <button>
-          <i></i>
-          <div>세미나실</div>
-        </button>
-      </CategoryItem>
-      
-      <CategoryItem>
-        <button>
-          <i></i>
-          <div>파티룸</div>
-        </button>
-      </CategoryItem>
-      
-      <CategoryItem>
-        <button>
-          <i></i>
-          <div>펜션</div>
-        </button>
-      </CategoryItem>
-      
-      <CategoryItem>
-        <button>
-          <i></i>
-          <div>호텔</div>
-        </button>
-      </CategoryItem>
-      
-      <CategoryItem>
-        <button>
-          <i></i>
-          <div>모텔</div>
-        </button>
-      </CategoryItem>
-      
-      <CategoryItem>
-        <button>
-          <i></i>
-          <div>차량 렌트/린스 승계</div>
-        </button>
-      </CategoryItem>
-      
-      <CategoryItem>
-        <button>
-          <i></i>
-          <div>가전</div>
-        </button>
-      </CategoryItem>
-      
-      <CategoryItem>
-        <button>
-          <i></i>
-          <div>스튜디오</div>
-        </button>
-      </CategoryItem>
-
-      
-      
-
+      <CategoryLineTop>
+        <CategoryIcon text="주거" />
+        <CategoryIcon text="사무실" />
+        <CategoryIcon text="회의실" />
+        <CategoryIcon text="연습실" />
+        <CategoryIcon text="세미나실" />
+        <CategoryIcon text="파티룸" />
+      </CategoryLineTop>
+      <CategoryLineBot>
+        <CategoryIcon text="펜션" />
+        <CategoryIcon text="호텔" />
+        <CategoryIcon text="모텔" />
+        <ForCarRent>
+          <CategoryIconImage></CategoryIconImage>
+          <CategoryIconText>차량 렌트/린스 승계</CategoryIconText>
+        </ForCarRent>
+        <CategoryIcon text="가전" />
+        <CategoryIcon text="스튜디오" />
+      </CategoryLineBot>
     </CategoryContainer>
-    
-  )
+  );
+};
 
+const CategoryContainer = styled.div`
+  width: 1250px;
+  height: 380px;
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
+const CategoryLineTop = styled.div`
+  width: 1070px;
+  height: 150px;
+`;
 
+const CategoryLineBot = styled.div`
+  width: 1070px;
+  height: 180px;
+  display: flex;
+  margin-top: 50px;
+`;
 
-}
+const ForCarRent = styled.div`
+  display: inline-block;
+  width: 150px;
+  height: 180px;
+  border: 1px solid;
+  margin: 0px 0px 0px 55px;
+`;
 
-const CategoryContainer = styled.section`
-  display : flex;
-  margin-top : 20vh;
-  flex-wrap: wrap;
-  justify-content: space-between;
-`
+const CategoryIconImage = styled.div`
+  width: 120px;
+  height: 120px;
+`;
 
-const CategoryItem = styled.div`
-
-  button {
-    
-    background: none;
-    color: inherit;
-    border: none;
-    padding: 0;
-    font: inherit;
-    cursor: pointer;
-    outline: inherit;
-
-    i {
-
-    }
-
-    div {
-      
-    }
-  }
-
-`
+const CategoryIconText = styled.div`
+  width: 150px;
+  height: 20px;
+  margin-top: 10px;
+  font-size: 20px;
+  text-align: center;
+`;
 
 export default Category;
