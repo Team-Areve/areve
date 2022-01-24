58bee0e8f93d32ed3f0165af993ededff2083fb5
K <dkh1001@outlook.kr>
2022-01-24 22:10:49 +0900

    Add LogIn Page

---
+++ src/pages/LogInPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+function LogInPage() {
+  return <Wrapper></Wrapper>;
+}
+
+const LogInLayout = styled.div``;
+
+const Wrapper = styled.div`
+  width: 600px;
+  height: 530px;
+`;
+
+export default LogInPage;
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { palette } from "../lib/styles/palette.js";
 import Header from "../components/main/Header";
+import axios from "axios";
 
 function RegisterPage() {
   // 비밀번호 규칙에 맞지 않으면 에러 메시지 띄우기
   // 형식에 안 맞을 때마다 인풋 밑에 살짝 메시지 띄우기 가능 스페이스 클라우드 처럼
+  // 실시간 감지
+
+  const [Email, SetEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [PasswordConfirm, setPasswordConfirm] = useState("");
+  const [Birth, setBirth] = useState("");
+  const [Phone, setPhone] = useState("");
+
+  const url = "";
+
+  const emailHandler = (e) => {
+    e.preventDefault();
+    SetEmail(e.target.value);
+  };
+  const passwordHandler = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  };
+  const passwordConfirmHandler = (e) => {
+    e.preventDefault();
+    setPasswordConfirm(e.target.value);
+  };
+  const birthHandler = (e) => {
+    e.preventDefault();
+    setBirth(e.target.value);
+  };
+  const phoneHandler = (e) => {
+    e.preventDefault();
+    setPhone(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(Email, Password, PasswordConfirm, Birth, Phone);
+
+    if (Password != PasswordConfirm) {
+      alert("pw dif");
+    }
+
+    let body = {
+      User_id: Email,
+      User_password: Password,
+      User_birth: Birth,
+      User_Phone: Phone,
+    };
+
+    axios.post(url, body).then((res) => console.log(res));
+  };
 
   return (
     <RegisterLayout>
@@ -14,29 +63,33 @@ function RegisterPage() {
         <Text>회원가입'^'</Text>
         <Form action="">
           <InputIndex>이메일</InputIndex>
-          <Input type="email" name="User_id" placeholder="이메일"></Input>
+          <Input
+            type="email"
+            placeholder="이메일"
+            onChange={emailHandler}
+          ></Input>
           <InputIndex>비밀번호</InputIndex>
           <Input
             type="password"
-            name="User_password"
+            onChange={passwordHandler}
             placeholder="비밀번호"
           ></Input>
           <Input
             style={{ marginTop: "20px" }}
             type="password"
-            name="User_password_confirm"
+            onChange={passwordConfirmHandler}
             placeholder="비밀번호 확인"
           ></Input>
           <InputIndex>생년월일</InputIndex>
-          <Input type="date" name="User_birth"></Input>
+          <Input type="date" onChange={birthHandler}></Input>
           <InputIndex>휴대전화</InputIndex>
           <Input
             style={{ width: "460px" }}
+            onChange={phoneHandler}
             type="text"
-            name="User_phonenumber"
           ></Input>
           <AuthrizePhone>휴대폰 인증</AuthrizePhone>
-          <Submit>가입하기</Submit>
+          <Submit onClick={submitHandler}>가입하기</Submit>
         </Form>
       </Wrapper>
     </RegisterLayout>
