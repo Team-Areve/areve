30a2a2fce7164404cd0721bf15633d4056b52149
이재하 <jhl2619@naver.com>
2022-01-27 21:32:34 +0900

    feat : Add routing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 import MainPage from './pages/MainPage';
 import GlobalStyles from 'lib/styles/globalStyles';
 import ApplyPage from 'pages/ApplyPage';
+import Routing from 'routes/Routing';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      {/* <MainPage /> */}
-      <ApplyPage></ApplyPage>
+      <Routing />
     </>
   );
 }
---
+++ src/routes/Routing.jsx
@@ -0,0 +1,17 @@
+import ApplyPage from 'pages/ApplyPage';
+import MainPage from 'pages/MainPage';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Path from './Path';
+
+function Routing() {
+  return (
+    <Routes>
+      <Route path={Path.MainPage} element={<MainPage />} />
+      <Route path={Path.ApplyPage} element={<ApplyPage />} />
+      {/* <Route path="*" element={<NotFoundPage />} /> */}
+    </Routes>
+  );
+}
+
+export default Routing;
