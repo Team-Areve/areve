62f8bd8c3a9040fa6da0b6ca6e7981595f0df551
K <dkh1001@outlook.kr>
2022-02-03 19:45:55 +0900

    For Backend Server Test

---
+++ userTest.py
@@ -0,0 +1,42 @@
+import random, requests, datetime
+
+url = ""
+
+for _ in range(3):
+  email = ""
+  l = random.randint(3, 15)
+  for j in range(l):
+    email += chr(random.randint(97, 113))
+
+  password = ""
+  l = random.randint(7, 15)
+  for j in range(l):
+    password += chr(random.randint(97, 113))
+
+  nickname = ""
+  l = random.randint(3, 15)
+  for j in range(l):
+    nickname += chr(random.randint(97, 113))
+
+  name = ""
+  l = random.randint(3, 15)
+  for j in range(l):
+    name += chr(random.randint(97, 113))
+
+  date = "2018-07-18"
+  now = datetime.datetime.now()
+  nowDate = now.strftime('%Y-%m-%d')
+
+  phone = "010"
+  for j in range(8):
+    phone += str(random.randint(0, 9))
+
+  body={"User_id": email,
+    "User_password": password,
+    "User_name": name,
+    "User_birth": date,
+    "User_phonenumber": phone,
+    "User_joindate": nowDate,
+  }
+  print(body)
+  requests.post(url, json=body)
