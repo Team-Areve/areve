85d246d8d7cbb109467fd7bed9b81d80868d9ab0
K <dkh1001@outlook.kr>
2022-02-17 16:52:10 +0900

    Fix Infinite Scroll

--- src/components/list/ItemList.jsx
+++ src/components/list/ItemList.jsx
@@ -1,35 +1,58 @@
 import Vertical from "components/item/Vertical";
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import Loader from "./Loader";
-import axios from "axios";
 import instance from "lib/Request";
 
 function ItemList(props) {
 	const [itemLists, setItemLists] = useState([]);
-	const req = instance;
+	const [isLoaded, setLoading] = useState(false);
+	const [target, setTarget] = useState(null);
+	let page = 0;
 
 	const getMoreItem = async () => {
+		setLoading(true);
 		await instance({
 			method: "get",
-			url: `/category/${props.catNum}/page/0`,
+			url: `/category/${props.catNum}/page/${page}`,
 		}).then((res) => {
+			if (res.status === 404) {
+				return;
+			}
 			setItemLists([...itemLists, ...res.data]);
+			page += 1;
 		});
+		setLoading(false);
+	};
+
+	const onIntersect = async ([entry], observer) => {
+		if (entry.isIntersecting && !isLoaded) {
+			observer.unobserve(entry.target);
+			await getMoreItem();
+			observer.observe(entry.target);
+		}
 	};
 
 	useEffect(() => {
-		getMoreItem();
-	}, []);
+		let observer;
+		if (target) {
+			observer = new IntersectionObserver(onIntersect, {
+				threshold: 0.4,
+			});
+			observer.observe(target);
+		}
+		return () => observer && observer.disconnect();
+	}, [target]);
 
 	return (
 		<ItemListContainer>
 			{itemLists.map((v, i) => {
 				return <Vertical key={v.itemnumber} item={v} cat={props.cat} />;
 			})}
-			{/* <div ref={setTarget} className="Target-Element">
-				{isLoaded && <Loader />}
-			</div> */}
+			<div
+				ref={setTarget}
+				className="Target-Element"
+				style={{ width: "1250px" }}
+			></div>
 		</ItemListContainer>
 	);
 }
