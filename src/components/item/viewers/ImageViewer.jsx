3f0dc10b5fb85587a811fee124082465c320bab5
K <dkh1001@outlook.kr>
2022-03-16 22:34:58 +0900

    Add Animation

--- src/components/item/viewers/ImageViewer.jsx
+++ src/components/item/viewers/ImageViewer.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 
 function ImageViewer({
 	itemnumber,
@@ -29,6 +29,11 @@ function ImageViewer({
 		navigate(`/item/${itemnumber}`);
 	};
 
+	const active = { opacity: "1", transition: "opacity 500ms" };
+	const hidden = {
+		display: "none",
+	};
+
 	return (
 		<ImageWrapper
 			width={width}
@@ -47,38 +52,32 @@ function ImageViewer({
 				src={images[curImg]}
 				style={{ borderRadius: radius.image }}
 			></Image>
-			{hover ? (
-				<HoverLeft
-					height={height}
-					style={{ borderRadius: radius.left }}
-					onClick={(e) => {
-						e.stopPropagation();
-						if (curImg === 0) {
-							setCurImg(cntImg - 1);
-						} else {
-							setCurImg((curImg - 1) % cntImg);
-						}
-					}}
-				>
-					＜
-				</HoverLeft>
-			) : (
-				<></>
-			)}
-			{hover ? (
-				<HoverRight
-					height={height}
-					style={{ borderRadius: radius.right }}
-					onClick={(e) => {
-						e.stopPropagation();
-						setCurImg((curImg + 1) % cntImg);
-					}}
-				>
-					＞
-				</HoverRight>
-			) : (
-				<></>
-			)}
+			<HoverLeft
+				height={height}
+				radius={radius.left}
+				hover={hover}
+				onClick={(e) => {
+					e.stopPropagation();
+					if (curImg === 0) {
+						setCurImg(cntImg - 1);
+					} else {
+						setCurImg((curImg - 1) % cntImg);
+					}
+				}}
+			>
+				＜
+			</HoverLeft>
+			<HoverRight
+				height={height}
+				radius={radius.right}
+				hover={hover}
+				onClick={(e) => {
+					e.stopPropagation();
+					setCurImg((curImg + 1) % cntImg);
+				}}
+			>
+				＞
+			</HoverRight>
 		</ImageWrapper>
 	);
 }
@@ -91,33 +90,63 @@ const ImageWrapper = styled.div`
 `;
 
 const HoverRight = styled.button`
-	width: 70px;
+	width: 80px;
 	height: ${(props) => props.height};
-	background: linear-gradient(to left, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0));
+	border-radius: ${(props) => props.radius};
+	background: linear-gradient(to left, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0));
 	z-index: 70;
 	position: absolute;
 	right: 0px;
 	top: 0px;
-	font-size: 30px;
+	font-size: 40px;
 	color: white;
 	line-height: ${(props) => props.height};
 	vertical-align: middle;
 	text-align: center;
+
+	${(props) => Animation(props.hover)}
 `;
 
 const HoverLeft = styled.button`
-	width: 70px;
+	width: 80px;
 	height: ${(props) => props.height};
-	background: linear-gradient(to right, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0));
+	border-radius: ${(props) => props.radius};
+	background: linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0));
 	z-index: 100;
 	position: absolute;
 	left: 0px;
 	top: 0px;
-	font-size: 30px;
+	font-size: 40px;
 	color: white;
 	line-height: ${(props) => props.height};
 	vertical-align: middle;
 	text-align: center;
+
+	${(props) => Animation(props.hover)}
+`;
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
+
+const Animation = (hover) => css`
+	visibility: ${hover ? "visible" : "hidden"};
+	animation: ${hover ? fadeIn : fadeOut} 0.3s ease-out;
+	transition: visibility 0.3s ease-out;
 `;
 
 const Image = styled.img`
