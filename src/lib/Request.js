ed80f1ebdba210f0d4ce7a5a797a2044675c31d9
K <dkh1001@outlook.kr>
2022-03-16 23:31:35 +0900

    Modify BaseUrl

--- src/lib/Request.js
+++ src/lib/Request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://areve.herokuapp.com",
 });
 
 instance.defaults.timeout = 2500;
