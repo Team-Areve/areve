4a067ec45744d5b7feb2b0374242f5c5a7da0c85
이재하 <jhl2619@naver.com>
2022-03-17 14:18:49 +0900

    feat : Add response from server

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,9 +1,8 @@
-import { SendBtn } from 'assets/icons';
 import Message from 'components/chat/Message';
 import Button from 'components/common/Button';
 import Input from 'components/common/Input';
 import { palette } from 'lib/styles/palette';
-import { FlexAlignCenter, FlexJustifyCenter } from 'lib/styles/utilStyles';
+import { FlexAlignCenter } from 'lib/styles/utilStyles';
 import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 import styled from 'styled-components';
@@ -14,29 +13,36 @@ const socket = io.connect('http://localhost:4000');
 
 function ChatPage() {
   const [text, setText] = useState('');
-  const [sendMsgList, setSendMsgList] = useState([]);
+  const [msgList, setMsgList] = useState([]);
   const onChange = (e) => {
     setText(e.target.value);
   };
-  const onClick = (e) => {
-    setSendMsgList([...sendMsgList, text]);
+  const onClick = () => {
+    const sendMsgObj = {
+      room_id: 'testRoom',
+      body: text,
+      user_id: 'sender',
+    };
+    socket.emit('front_to_back', sendMsgObj);
     setText('');
-    socket.emit('chatting', sendMsgList);
   };
-  const onRenderChat = () => {
-    return sendMsgList.map((msg, idx) => (
-      <Message key={`sendMsg_${idx}`} variant="send">
-        {msg}
-      </Message>
-    ));
-  };
-
   useEffect(() => {
-    // socket.to('some room').emit('some event');
-    socket.on('chatting', (data) => {
-      console.log(data);
+    socket.emit('room', 'testRoom');
+
+    socket.on('back_to_front', (msgObj, time) => {
+      setMsgList([...msgList, msgObj.body]);
+      console.log(msgList);
+      // if (msgList[-1].body !== msgObj.body) {
+      //   setMsgList(...msgList, msgObj);
+      // }
     });
-  }, []);
+
+    // socket.to('some room').emit('some event');
+    // socket.on('chatting', (data) => {
+    //   console.log(data);
+    //   setReceiveMsgList(...receiveMsgList, data);
+    // });
+  }, [msgList]);
 
   return (
     <ChatPageContainer>
@@ -44,7 +50,11 @@ function ChatPage() {
         <ChatPageOpponent>상대방</ChatPageOpponent>
         <ChatPageClose>X</ChatPageClose>
       </ChatPageTop>
-      <ChatPageChattingList>{onRenderChat()}</ChatPageChattingList>
+      <ChatPageChattingList>
+        {msgList.map((msg) => (
+          <Message variant="send">{msg}</Message>
+        ))}
+      </ChatPageChattingList>
 
       <ChatPageInputWrapper>
         <ChatPageMenuBtn src={PlusBtnImg} />
