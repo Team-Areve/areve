dfb426d0345593ae2f7643872c73f4ed5f7df271
K <dkh1001@outlook.kr>
2022-05-01 15:28:44 +0900

    x

--- src/lib/Request.js
+++ src/lib/Request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://areve.herokuapp.com",
+	baseURL: "http://193.122.121.118:8000",
 });
 
 instance.defaults.timeout = 2500;
